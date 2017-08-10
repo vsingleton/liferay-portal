1dfe5ebd02ffd699a012a86b133c60ace902e7b6
Dante Wang <dante.wang@liferay.com>
2017-08-10 14:34:19 +0800

    LRQA-33560 Squashed: Portlet 3.0 TCK

--- modules/test/portlet-tck-bridge/src/main/java/com/liferay/portlet/tck/bridge/deploy/auto/TCKPortletAutoDeployListener.java
+++ modules/test/portlet-tck-bridge/src/main/java/com/liferay/portlet/tck/bridge/deploy/auto/TCKPortletAutoDeployListener.java
@@ -24,6 +24,7 @@ import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Miguel Pastor
+ * @author Vernon Singleton
  */
 @Component(immediate = true, service = AutoDeployListener.class)
 public class TCKPortletAutoDeployListener extends PortletAutoDeployListener {
