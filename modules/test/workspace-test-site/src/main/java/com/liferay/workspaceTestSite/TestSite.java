fbeb8c07af440fb46c1f0d006820d89ef605a139
vsingleton <vsingleton@gmail.com>
2018-07-24 18:24:52 -0400

    Initial commit of workspace-test-site module

---
+++ modules/test/workspace-test-site/src/main/java/com/liferay/workspaceTestSite/TestSite.java
@@ -0,0 +1,264 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.workspaceTestSite;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+import com.liferay.petra.log4j.Log4JUtil;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
+import com.liferay.portal.kernel.util.ThreadUtil;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.struts.StrutsActionRegistryUtil;
+import com.liferay.workspaceTestSite.configuration.TestSiteConfiguration;
+import com.liferay.workspaceTestSite.setup.Setup;
+import com.liferay.workspaceTestSite.struts.TestSiteStrutsAction;
+
+/**
+ * @author Matthew Tambara
+ * @author Vernon Singleton
+ */	
+@Component(
+	configurationPid = "com.liferay.workspaceTestSite.configuration.TestSiteConfiguration",
+	configurationPolicy = ConfigurationPolicy.REQUIRE
+)
+public class TestSite {
+
+	@Activate
+	@Modified
+	protected void activate(ComponentContext componentContext) {
+		deactivate();
+
+		if (!_log.isInfoEnabled()) {
+			Log4JUtil.setLevel("com.liferay.workspaceTestSite", "INFO", true);
+		}
+
+		TestSiteConfiguration testSiteConfiguration =
+			ConfigurableUtil.createConfigurable(
+				TestSiteConfiguration.class,
+				componentContext.getProperties());
+		
+//		System.err.println("activate: using PortletLocalServiceUtil.getPortlets ...");
+//		List<Portlet> ps = PortletLocalServiceUtil.getPortlets();
+//		for (Portlet portlet : ps) {
+//			String portletName = portlet.getPortletName();
+//			String contextName = portlet.getContextName();
+//			if (Pattern.compile(Pattern.quote("sample"), Pattern.CASE_INSENSITIVE).matcher(portletName).find() ||
+//				Pattern.compile(Pattern.quote("sample"), Pattern.CASE_INSENSITIVE).matcher(contextName).find()) {
+//				System.err.println("activate: portlet.getPortletName() = " + portlet.getPortletName() + " contextName = " + portlet.getContextName());
+//			}
+//		}
+
+		// Wait for portlet wars to deploy ...
+		// They need to be available before setting up sites and pages
+		
+		String jsonFile = testSiteConfiguration.jsonFile();
+		System.out.println("activate: jsonFile = " + jsonFile);
+		
+		HashMap<String, String> portletMap = new HashMap<String, String>();
+		
+		try {
+			
+			byte[] bytes = Files.readAllBytes(Paths.get(jsonFile));
+			String content = new String(bytes, StandardCharsets.UTF_8);
+			JSONObject jsonObject = new JSONObject(content);
+			JSONObject site = (JSONObject) jsonObject.get("site");		
+			JSONArray pages = site.getJSONArray("pages");
+			
+			for (int p = 0; p < pages.length(); p++) {
+			    JSONObject page = (JSONObject) pages.get(p);
+			    String name = page.getString("name");
+//			    System.out.println("activate: page name = " + name);
+			    
+			    JSONArray portlets = page.getJSONArray("portlets");
+			    
+			    for (int o = 0; o < portlets.length(); o++) {
+			    	String portlet = portlets.get(o).toString();
+//			    	System.out.println("activate: portlet = " + portlet);
+			    	portletMap.put(portlet, "1");
+			    }
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Set<String> portletSet = portletMap.keySet();
+	    for (String portlet : portletSet) {
+	    	System.out.println("activate: setting up wait for portlet = " + portlet);
+	    	_waitForDeployment(
+				portlet, System.currentTimeMillis(),
+				testSiteConfiguration.timeout() * Time.SECOND);
+	    }
+
+	    Setup.setupTestSite(testSiteConfiguration);
+
+		StrutsActionRegistryUtil.register(_PATH, new TestSiteStrutsAction());
+
+		FutureTask<Void> futureTask = new FutureTask<>(
+			new HandshakeServerCallable(testSiteConfiguration));
+
+		_handshakeServerFuture = futureTask;
+
+		Thread handshakeServerThread = new Thread(
+			futureTask, "Handshake server thread");
+
+		handshakeServerThread.setDaemon(true);
+
+		handshakeServerThread.start();
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		Future<Void> handshakeServerFuture = _handshakeServerFuture;
+
+		if (handshakeServerFuture != null) {
+			handshakeServerFuture.cancel(true);
+		}
+		
+		StrutsActionRegistryUtil.unregister(_PATH);
+	}
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+	private void _waitForDeployment(
+		String waitingForName, long startTime, long timeout) {
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"_waitForDeployment: of " +
+					waitingForName + " ...");
+		}
+		
+		while ((System.currentTimeMillis() - startTime) < timeout) {
+			
+			boolean found = false;
+			
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"_waitForDeployment: of " +
+						waitingForName + " ...");
+			}
+			
+			List<Portlet> portlets = PortletLocalServiceUtil.getPortlets();
+			for (Portlet portlet : portlets) {
+				String portletName = portlet.getPortletName();
+				found = portletName.equals(waitingForName) && portlet.isActive();
+				if (found) {
+					break;
+				}
+			}
+
+			if (found) {
+				if (_log.isInfoEnabled()) {
+					_log.info(
+						"_waitForDeployment: found " +
+							waitingForName);
+				}
+
+				return;
+			}
+			else {
+				try {
+					Thread.sleep(250);
+				}
+				catch (InterruptedException ie) {
+					if (_log.isWarnEnabled()) {
+						_log.warn(ie.getMessage());
+					}
+				}
+			}
+		}
+
+		_log.error("Timeout waiting for " + waitingForName);
+		_log.error(ThreadUtil.threadDump());
+	}
+
+	private static final String _PATH = "/test/site";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		TestSite.class);
+
+	private volatile Future<Void> _handshakeServerFuture;
+
+	private static class HandshakeServerCallable implements Callable<Void> {
+
+		@Override
+		public Void call() throws IOException {
+			try (ServerSocket serverSocket = new ServerSocket(
+					_testSiteConfiguration.handshakeServerPort())) {
+
+				serverSocket.setSoTimeout(250);
+
+				while (!Thread.interrupted()) {
+					try (Socket socket = serverSocket.accept();
+						OutputStream outputStream = socket.getOutputStream()) {
+
+						outputStream.write(
+							"Test Site is ready".getBytes(
+								Charset.defaultCharset()));
+					}
+					catch (SocketTimeoutException ste) {
+					}
+				}
+			}
+
+			return null;
+		}
+
+		private HandshakeServerCallable(
+			TestSiteConfiguration testSiteConfiguration) {
+
+			_testSiteConfiguration = testSiteConfiguration;
+		}
+
+		private final TestSiteConfiguration
+			_testSiteConfiguration;
+
+	}
+
+}
---
+++ modules/test/workspace-test-site/src/main/java/com/liferay/workspaceTestSite/setup/Setup.java
@@ -0,0 +1,434 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.workspaceTestSite.setup;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.osgi.framework.Bundle;
+
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.NoSuchGroupException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.LayoutTypePortlet;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.RoleConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
+import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
+import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.workspaceTestSite.configuration.TestSiteConfiguration;
+
+/**
+ * @author Vernon Singleton
+ */
+public class Setup {
+
+	public static void setupTestSite(
+		TestSiteConfiguration testSiteConfiguration) {
+
+		long companyId = 0L;
+		long userId = 0L;
+
+		try {
+			for (long id : PortalUtil.getCompanyIds()) {
+				_setupPermissionChecker(id);
+				Company company = CompanyLocalServiceUtil.getCompanyById(id);
+
+				if ("Liferay".equals(company.getName())) {
+					companyId = id;
+					userId = company.getDefaultUser().getUserId();
+				}
+			}
+			
+			System.err.println("wts setupTestSite: companyId = " + companyId);
+			System.err.println("wts setupTestSite: userId = " + userId);
+
+			Group group = _getGroupForSite(companyId, userId);
+
+			long groupId = group.getGroupId();
+
+			_addAllUsersToSite(groupId);
+			
+			String jsonFile = testSiteConfiguration.jsonFile();
+			
+			try {
+				
+				byte[] bytes = Files.readAllBytes(Paths.get(jsonFile));
+				String content = new String(bytes, StandardCharsets.UTF_8);
+				JSONObject jsonObject = new JSONObject(content);
+				JSONObject site = (JSONObject) jsonObject.get("site");		
+				JSONArray pages = site.getJSONArray("pages");
+				
+				for (int p = 0; p < pages.length(); p++) {
+				    JSONObject page = (JSONObject) pages.get(p);
+				    String pageName = page.getString("name");
+				    System.out.println("activate: pageName = " + pageName);
+				    
+				    JSONArray jsonArrayOfPortlets = page.getJSONArray("portlets");
+
+				    List<Portlet> portlets = new LinkedList<>();
+			    	for (int i = 0; i < jsonArrayOfPortlets.length(); i += 1) {
+			    		String portlet = jsonArrayOfPortlets.get(i).toString();
+						portlets.add(_createPortlet(portlet, pageName));
+			    	}
+			    	
+			    	PortalPage portalPage = new PortalPage(pageName, portlets);
+
+					_setupPage(userId, groupId, portalPage, pageName);
+
+				}
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		catch (Exception e) {
+			_log.error(e.getMessage(), e);
+		}
+	}
+
+	private static void _addAllUsersToSite(long groupId) throws Exception {
+		List<User> users = UserLocalServiceUtil.getUsers(
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+		ArrayList<Long> userIdList = new ArrayList<>();
+
+		for (User user : users) {
+			if (!user.isDefaultUser()) {
+				userIdList.add(user.getUserId());
+			}
+		}
+
+		long[] userIds = new long[userIdList.size()];
+
+		for (int i = 0; i < userIds.length; i++) {
+			userIds[i] = userIdList.get(i);
+		}
+
+		UserLocalServiceUtil.addGroupUsers(groupId, userIds);
+	}
+
+	private static void _addPortlet(
+			LayoutTypePortlet layoutTypePortlet, long userId, int columnNumber,
+			String portletId)
+		throws PortalException {
+
+		String columnNumberLabel = Integer.toString(columnNumber);
+
+		// Liferay 6.2 changed the expected value for the String-based column
+		// number. Previous versions didn't require the "column-" prefix.
+
+		columnNumberLabel = "column-" + columnNumber;
+
+		// NOTE: In Liferay 6.1.x the following call was to setPortletIds()
+		// but that method was removed in 6.2.x
+
+		layoutTypePortlet.addPortletId(
+			userId, portletId, columnNumberLabel, -1);
+
+		layoutTypePortlet.resetModes();
+		layoutTypePortlet.resetStates();
+	}
+
+	private static Portlet _createPortlet(String portletName, String pageName) {
+		return new Portlet(portletName, pageName, false);
+	}
+
+	private static Group _getGroupForSite(long companyId, long userId)
+		throws Exception {
+
+		Group group;
+		String name = "Workspace Test Site";
+
+		try {
+			group = GroupLocalServiceUtil.getGroup(companyId, name);
+		}
+		catch (NoSuchGroupException nsge) {
+			group = ServiceUtil.addActiveOpenGroup(userId, name);
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"Setting up site name=[" + group.getName() +
+					"] publicLayouts=[" + group.hasPublicLayouts() + "]");
+		}
+
+		return group;
+	}
+
+	private static Layout _getPortalPageLayout(
+			long userId, long groupId, String portalPageName)
+		throws Exception {
+
+		Layout portalPageLayout = null;
+
+		List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(groupId, true);
+
+		for (Layout layout : layouts) {
+			if (layout.getName(Locale.US).equals(portalPageName)) {
+				portalPageLayout = layout;
+			}
+		}
+
+		if (portalPageLayout == null) {
+			long parentLayoutId = LayoutConstants.DEFAULT_PARENT_LAYOUT_ID;
+			String type = LayoutConstants.TYPE_PORTLET;
+			String friendlyURL = "/" + StringUtil.toLowerCase(portalPageName);
+
+			portalPageLayout = ServiceUtil.addLayout(
+				userId, groupId, true, parentLayoutId, portalPageName,
+				portalPageName, portalPageName, type, false, friendlyURL);
+		}
+
+		return portalPageLayout;
+	}
+
+	private static void _setupPage(
+			long userId, long groupId, PortalPage portalPage,
+			String pageName)
+		throws Exception {
+
+		Bundle[] bundles = BundleUtil.getBundles();
+
+		String portalPageName = portalPage.getPageName();
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("setupPage: portalPageName = " + portalPageName);
+		}
+
+		List<Portlet> portlets = portalPage.getPortlets();
+		Layout portalPageLayout = _getPortalPageLayout(
+			userId, groupId, portalPageName);
+
+		LayoutTypePortlet layoutTypePortlet =
+			(LayoutTypePortlet)portalPageLayout.getLayoutType();
+
+		layoutTypePortlet.setLayoutTemplateId(
+			userId, portalPage.getLayoutTemplateId(), false);
+
+		int columnNumber = 1;
+
+		List<String> addedPortletIds = new ArrayList<>();
+
+		for (Portlet portlet : portlets) {
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"setupPage: searching for servetContextName = " +
+						pageName);
+			}
+
+			long bundleId = 0L;
+			int bundleState = Bundle.UNINSTALLED;
+
+			for (Bundle bundle : bundles) {
+				String symbolicName = bundle.getSymbolicName();
+
+				// TODO remove bundle symbolicName magic.
+
+				if (symbolicName.startsWith(pageName)) {
+					bundleId = bundle.getBundleId();
+					bundleState = bundle.getState();
+				}
+			}
+
+			String portletName = portlet.getPortletName();
+
+			if (_log.isDebugEnabled()) {
+				_log.debug("setupPage:  final bundleId = " + bundleId);
+			}
+
+			if (bundleId > 0) {
+				if (bundleState == Bundle.ACTIVE) {
+					_addPortletsToPage(portletName, portlet, pageName, layoutTypePortlet, userId, columnNumber, addedPortletIds, false);
+				}
+				else {
+					_log.error(
+						"Unable to add portletName=[" + portletName +
+							"] since bundle =[" + pageName +
+								"] is not active.");
+				}
+			}
+			else {
+				_log.info("There is no BundleName=[" + pageName + "] that is deployed ...");
+				_addPortletsToPage(portletName, portlet, pageName, layoutTypePortlet, userId, columnNumber, addedPortletIds, true);
+			}
+
+			// columnNumber++;
+
+		}
+
+		LayoutLocalServiceUtil.updateLayout(portalPageLayout);
+
+//		if (_log.isDebugEnabled()) {
+//			_log.debug("setupPage: groupId = " + groupId);
+//			_log.debug(
+//				"setupPage: portalPageLayout.getLayoutId() = " +
+//					portalPageLayout.getLayoutId());
+//			_log.debug("setupPage: themeId = portlettck_WAR_potlettcktheme");
+//			_log.debug(
+//				"setupPage: portalPageLayout.getColorSchemetId() = " +
+//					portalPageLayout.getColorSchemeId());
+//			_log.debug("setupPage: css = ");
+//			_log.debug(
+//				"setupPage: LayoutLocalServiceUtil.updateLookAndFeel ...");
+//		}
+//
+//		LayoutSetLocalServiceUtil.updateLookAndFeel(
+//			groupId, true, "portlettck_WAR_portlettcktheme",
+//			portalPageLayout.getColorSchemeId(), null);
+
+		int totalAddedPortlets = addedPortletIds.size();
+
+		if (totalAddedPortlets == 0) {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"Setting up page name=[" + portalPageName +
+						"] without any portlets");
+			}
+		}
+		else if (totalAddedPortlets == 1) {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"Setting up page name=[" + portalPageName +
+						"] with portlet=" +
+							addedPortletIds.toString());
+			}
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"Setting up page name=[" + portalPageName +
+						"] with portlets=" +
+							addedPortletIds.toString());
+			}
+		}
+	}
+	
+	private static void _addPortletsToPage(String portletName, Portlet portlet, String pageName, LayoutTypePortlet layoutTypePortlet, long userId, int columnNumber, List<String> addedPortletIds, boolean usePortletName) {
+		
+		// Apparently newer style portlets using the @Component annotation gets a portletId as follows ...
+		String portletId = portletName + portlet.getInstanceToken();
+		System.err.println("_addPortletsToPage: portletId = " + portletId);
+		com.liferay.portal.kernel.model.Portlet p = PortletLocalServiceUtil.getPortletById(portletId);
+		
+		if (p == null) {
+//			System.err.println("_setupPage: could not get Portlet by portletId = " + portletId + " ... p == null");
+			
+			// But a normal old WAR gets a portletId with _WAR_ in it as below
+			if (portlet.getArtifactType() == Portlet.ArtifactType.WAB) {
+				portletId = portletName + portlet.getInstanceToken();
+			}
+			else {
+				String noDashPortletName = portletName.replaceAll(
+					"[-]", "");
+
+				String name = pageName;
+				if (usePortletName) {
+					name = portletName;
+				}
+				String warContext = "_WAR_" + name;
+
+				portletId =
+					noDashPortletName +
+						warContext.replaceAll("[.]", "") +
+							portlet.getInstanceToken();
+			}
+		} else {
+			// newer style portlets using the @Component annotation ... we already have the portletId
+			String instanceId = p.getInstanceId();
+			System.err.println("_addPortletsToPage: instanceId = " + instanceId);
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"adding portletId=[" + portletId +
+					"] portletName=[" + portletName + "] ...");
+		}
+
+		try {
+			_addPortlet(
+				layoutTypePortlet, userId, columnNumber, portletId);
+		} catch (PortalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// not needed for Liferay 7
+		// storePortletPreferences(portalPageLayout, portletId);
+
+		addedPortletIds.add(portletId);
+	}
+
+	/**
+	 * This method sets up the {@link PermissionChecker} {@link ThreadLocal}
+	 * prior to performing additional test setup.
+	 */
+	private static void _setupPermissionChecker(long companyId)
+		throws PortalException {
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("setupPermissionChecker: companyId = " + companyId);
+		}
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		if (permissionChecker == null) {
+			Role administratorRole = RoleLocalServiceUtil.getRole(
+				companyId, RoleConstants.ADMINISTRATOR);
+
+			User administratorUser = UserLocalServiceUtil.getRoleUsers(
+				administratorRole.getRoleId()).get(0);
+
+			try {
+				permissionChecker = PermissionCheckerFactoryUtil.create(
+					administratorUser);
+
+				PermissionThreadLocal.setPermissionChecker(permissionChecker);
+			}
+			catch (Exception e) {
+				throw new SystemException(e);
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(Setup.class);
+
+}
